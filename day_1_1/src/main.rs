30e81f1fc715b46ad62b639d569797661bb768ff
Björn Guth <scatty@bguth.de>
2020-12-17 19:14:24 +0100

    Create solution for part 2 of day 1

--- day_1_1/src/main.rs
+++ day_1_1/src/main.rs
@@ -7,11 +7,21 @@ fn main() -> std::io::Result<()>{
     file.read_to_string(&mut content)?;
     let string_vec = content.split("\n").collect::<Vec<&str>>();
     let number_iter = string_vec.iter().map(|s| s.to_string().parse::<u32>().unwrap_or_default());
-    let number_vec = number_iter.collect::<Vec<u32>>();
+    let total_number_vec = number_iter.collect::<Vec<u32>>();
+    let number_vec = &total_number_vec[..total_number_vec.len()-1];
     for i in 0..number_vec.len()-1 {
         for j in &number_vec[i+1..] {
             if number_vec[i] + j == 2020 {
-                println!("{} * {} = {}", number_vec[i], j, number_vec[i]*j);
+                println!("Solution 1: {} * {} = {}", number_vec[i], j, number_vec[i]*j);
+            }
+        }
+    }
+    for i in 0..number_vec.len()-2 {
+        for j in i..number_vec.len()-1 {
+            for k in &number_vec[j+1..] {
+                if number_vec[i] + number_vec[j] + k == 2020 {
+                    println!("Solution 2: {} * {} * {} = {}", number_vec[i], number_vec[j], k, number_vec[i]*number_vec[j]*k);
+                }
             }
         }
     }
