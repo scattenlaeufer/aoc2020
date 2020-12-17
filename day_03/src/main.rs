72f773af13af02adc8eb9571595bb52c30092485
Björn Guth <scatty@bguth.de>
2020-12-17 22:07:37 +0100

    Create solution for part 2 of day 3

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     let mut pos = 0;
     let tree = "#";
 
-    for line in map {
+    for line in map.iter() {
         if line[pos % line.len()] == tree {
             trees_hit += 1;
         }
@@ -25,4 +25,25 @@ fn main() {
     }
 
     println!("trees hit: {}", trees_hit);
+
+    let slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]];
+    let mut pos_vec = vec![0, 0, 0, 0, 0];
+    let mut trees_hit_vec = vec![0, 0, 0, 0, 0];
+
+    for i in 0..map.len() {
+        for j in 0..slopes.len() {
+            if i % slopes[j][0] == 0 {
+                if map[i][pos_vec[j] % map[i].len()] == tree {
+                    trees_hit_vec[j] += 1;
+                }
+                pos_vec[j] += slopes[j][1];
+            }
+        }
+    }
+
+    let mut trees_hit: i64 = 1;
+    for h in trees_hit_vec {
+        trees_hit = trees_hit * h;
+    }
+    println!("trees hit on all slopes multiplied: {}", trees_hit);
 }
