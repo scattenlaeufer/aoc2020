21b01beae96b9cdcf3a59873a3eaccf310965cda
Björn Guth <scatty@bguth.de>
2020-12-17 18:55:36 +0100

    Create first solution for day 1

--- day_1_1/src/main.rs
+++ day_1_1/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()>{
+    let mut file = File::open("input")?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    let string_vec = content.split("\n").collect::<Vec<&str>>();
+    let number_iter = string_vec.iter().map(|s| s.to_string().parse::<u32>().unwrap_or_default());
+    let number_vec = number_iter.collect::<Vec<u32>>();
+    for i in 0..number_vec.len()-1 {
+        for j in &number_vec[i+1..] {
+            if number_vec[i] + j == 2020 {
+                println!("{} * {} = {}", number_vec[i], j, number_vec[i]*j);
+            }
+        }
+    }
+
+    Ok(())
 }
