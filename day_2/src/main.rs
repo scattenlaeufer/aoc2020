e096703dfd5aba4d12a59b3df514b678f252b2a6
Björn Guth <scatty@bguth.de>
2020-12-17 20:26:36 +0100

    Create a solution for part 1 of day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("input").unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+    let string_vec = content.split("\n").collect::<Vec<&str>>();
+
+    let mut valid_passwords = 0;
+
+    for rule_string in &string_vec[..string_vec.len()-1] {
+        let input_split = rule_string.split(": ").collect::<Vec<&str>>();
+        let password = input_split[1];
+        let rule = input_split[0].split(" ").collect::<Vec<&str>>();
+        let letter = rule[1];
+        let letter_amount = rule[0].split("-").collect::<Vec<&str>>();
+        let low = letter_amount[0].to_string().parse::<usize>().unwrap();
+        let high = letter_amount[1].to_string().parse::<usize>().unwrap();
+        let amount = password.matches(letter).count();
+        if low <= amount && amount <= high {
+            valid_passwords += 1;
+        }
+    }
+
+    println!("valid passwords: {}", valid_passwords);
 }
